60d713a80967306b3f2efdaa6cd9f4aa2ce4a025
Sofía Rivas Muñoz <sofi.rivasm@gmail.com>
2016-03-14 21:30:16 -0600

    URLs por módulo

---
+++ cuentas/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+
+    url(r'^login/$', 
+    	views.LoginView.as_view(),
+    	name='login'
+    	),
+
+    url(r'^registro/$', 
+    	views.RegistroView.as_view(),
+    	name='registro'
+    	),
+
+]
--- redsocial/settings.py
+++ redsocial/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'redsocial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
